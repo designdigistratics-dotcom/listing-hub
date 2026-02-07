3c11174aca64b0fac4e4b0324e50e589b1437daa
conquere-of-the-world <nkumarsingh378@gmail.com>
2026-02-08 01:30:55 +0530

    Pre launch

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.digitaloceanspaces.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'topickx.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.topickx.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
